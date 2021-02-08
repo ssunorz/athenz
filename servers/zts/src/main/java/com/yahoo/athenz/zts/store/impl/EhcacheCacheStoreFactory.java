57965316b44bcae3347dee7438e4125fc65ca486
ssuno <ssuno@yahoo-corp.jp>
2021-02-08 15:08:29 +0900

    Add register

--- servers/zts/src/main/java/com/yahoo/athenz/zts/store/impl/EhcacheCacheStoreFactory.java
+++ servers/zts/src/main/java/com/yahoo/athenz/zts/store/impl/EhcacheCacheStoreFactory.java
@@ -142,6 +142,8 @@ public class EhcacheCacheStoreFactory implements CacheStoreFactory {
             kryo.register(GroupAuditLog.class);
 
             kryo.register(Timestamp.class);
+
+            kryo.register(AssertionEffect.class);
         }
 
         public DataCacheSerializer(ClassLoader cl) {
